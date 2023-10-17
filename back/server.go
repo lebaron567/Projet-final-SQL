d591653907e11d45fd79e16fb80038b7aaf38971
Emeric barotin <emericbarotin@etu.univ-nantes.fr>
2023-10-17 14:12:19 +0200

    :rocket: | déploment serveur

--- back/server.go
+++ back/server.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
 
+var home = template.Must(template.ParseFiles("template/home.html"))
+
+func main() {
+	http.HandleFunc("/home", Home)
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	fmt.Println("Serveur start at : http://localhost:8080/home")
+	http.ListenAndServe(":8080", nil)
 }
+
+func Home(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/home", http.StatusFound)
+	}
