c91d4c7e63c8a2240d037c929d2fb8d1ce0f570a
Emeric barotin <emericbarotin@etu.univ-nantes.fr>
2023-10-17 15:13:53 +0200

    :construction: | Work in progress.

--- back/server.go
+++ back/server.go
@@ -10,12 +10,17 @@ var home = template.Must(template.ParseFiles("template/home.html"))
 
 func main() {
 	http.HandleFunc("/home", Home)
-	fs := http.FileServer(http.Dir("assets/"))
-	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	fmt.Println("Serveur start at : http://localhost:8080/home")
-	http.ListenAndServe(":8080", nil)
+	// fs := http.FileServer(http.Dir("assets/"))
+	// http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	fmt.Println("Serveur start at : http://localhost:8000/home")
+	http.ListenAndServe(":8000", nil)
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/home", http.StatusFound)
+
+		err := home.Execute(w, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	}
